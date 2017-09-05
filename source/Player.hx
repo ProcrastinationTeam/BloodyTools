2f1d6873601cc71ccac3c3665ce403578defb93e
LeRyokan <tixier.lucas@gmail.com>
2017-09-05 16:54:56 +0200

    Some rework
    Generation des niveaux basé sur une image

--- source/Player.hx
+++ source/Player.hx
@@ -3,34 +3,56 @@ package;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
+import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxPoint;
 import flixel.math.FlxVelocity;
 import flixel.system.FlxSound;
+import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import utils.Tweaking;
 import flixel.math.FlxPoint;
 
 class Player extends FlxSprite
 {
-	
+
 	public var doubleJump :Int = 0;
 	public var JumpC :Int = 0;
 	public var jumpTimer : FlxTimer;
 	public var timerManager : FlxTimerManager;
+	public var jumpingVelocity : Int = 600;
+	public var maxJumpHeight : Float  = 200.0;
+	public var currentHeight : Float ;
+	public var initialHeight : Float ;
+	public var maxHeightReach : Bool  = false;
+	public var isJumping 		:Bool = false;
+
+	//2nd MOVE
+	public var canDoubleJump : Bool = false;
+	public var jumpC :Int = 0;
+	public var horizontalSpeed : Int = 1000;
+	public var buttonJumpRelease : Bool = false;
+
+	
+	//Weapon system
+	public var weaponSprite : FlxSprite;
+	
 	
 	public function new(?X:Float=0, ?Y:Float=0)
 	{
 		super(X, Y);
+
+		//DEBUG SECTION WATCHER
+		FlxG.watch.add(this, "canDoubleJump", "canDoubleJump");
+		FlxG.watch.add(this, "jumpC", "JumpCount");
 		
-		FlxG.watch.add(this, "doubleJump", "dJump");
+		//TIMER DE TEST POUR LE SAUT
 		timerManager = new FlxTimerManager();
 		jumpTimer = new FlxTimer();
-		
 
+		//CHARGEMENT DU SPRITE ET SES ANIMS
 		loadGraphic(Tweaking.playerSprite, true, 16, 16);
 		setFacingFlip(FlxObject.RIGHT, false, false);
 		setFacingFlip(FlxObject.LEFT, true, false);
-
 		animation.add("idle", [0], 10, true);
 		animation.add("walk", [1], 6, true);
 		animation.add("crouch", [2], 30, true);
@@ -38,35 +60,57 @@ class Player extends FlxSprite
 		animation.add("jump", [3], 30, true);
 		animation.add("fall", [4], 30, true);
 		animation.add("afterfall", [5], 30, true);
+		
+		
 
 		/* A MODIFIER */
 		maxVelocity.x = 400;
 		maxVelocity.y = 2000;
-		
+
 		drag.x = maxVelocity.x * 8;
 		acceleration.y = 3000;
 
 		//a rebuild
-		scale = new FlxPoint(3, 3);
+		scale = new FlxPoint(2, 2);
 		//updateHitbox();
-		setSize(6 * 3, 3 * 3);
-		offset.set(0, 22);
+		setSize(6 * 2, 3 * 2);
+		offset.set(2, 18);
+		
+		
+		//WEAPON SYSTEM INIT
+		
+		weaponSprite = new FlxSprite(this.x, this.y);
+		weaponSprite.makeGraphic(16, 16, FlxColor.RED, false);
+		weaponSprite.visible = false;
+		//weaponSprite.loadGraphic(Tweaking.weaponSprite, true, 16, 16);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 	}
 
 	override public function update(elapsed:Float):Void
 	{
-		movement();
+		// Version 1 du move
+		//movement();
 
+		move();
+		//weaponSprite.setPosition(this.x, this.y);
 		super.update(elapsed);
 	}
-	
-	
+
 	public function onTimer(timer:FlxTimer):Void
 	{
 		trace("HELLO");
-		
 	}
-	
+
 	/**
 	 * Gestion des mouvements
 	 */
@@ -76,15 +120,20 @@ class Player extends FlxSprite
 		var moveDown:Bool = FlxG.keys.anyPressed([Tweaking.moveDown]);
 		var moveLeft:Bool = FlxG.keys.anyPressed([Tweaking.moveLeft]);
 		var moveRight:Bool = FlxG.keys.anyPressed([Tweaking.moveRight]);
-		
-		
+
 		acceleration.x = 0;
-		
+
 		if (isTouching(FlxObject.FLOOR))
 		{
 			doubleJump = 0;
+			if (isJumping)
+			{
+				isJumping = false;
+				animation.play("afterfall");
+			}
+
 		}
-		
+
 		if (moveLeft)
 		{
 			acceleration.x -= maxVelocity.x * 6;
@@ -95,40 +144,43 @@ class Player extends FlxSprite
 			acceleration.x += maxVelocity.x * 6;
 			facing = FlxObject.RIGHT;
 		}
-		
-		if (FlxG.keys.anyJustPressed([Tweaking.moveUp]))
-		{
-			trace(" Debut du Jump");
-			//jumpTimer.active = true;
-			jumpTimer.start(100);
-			
-		}
-		
+
 		if (moveDown)
 		{
 			animation.play("crouch");
 		}
-		
-		
-	if (FlxG.keys.anyPressed([Tweaking.moveUp]))
+
+		/* JUMP */
+
+		if (FlxG.keys.anyJustPressed([Tweaking.moveUp]))
 		{
-			
-			trace(jumpTimer.elapsedTime);
-			
-			if (jumpTimer.elapsedTime > 0.2)
-			{
-			
-			}
-			
-			
+			trace(" Debut du Jump");
+			jumpTimer.start(100);
+			initialHeight = this.y;
+			isJumping = true;
+
+		}
+
+		if (FlxG.keys.anyPressed([Tweaking.moveUp]))
+		{
+
 			if ((isTouching(FlxObject.FLOOR) || doubleJump < 2) && JumpC == 0)
 			{
-				velocity.y = -maxVelocity.y / 2;
+				//velocity.y = -maxVelocity.y / 2;
+				velocity.y = -jumpingVelocity;
+				JumpC++;
 			}
-			JumpC++;
-			
-			
+
+			//trace("Jump Height : " + -(this.y - initialHeight));
+			/*	if (maxJumpHeight < -(this.y - initialHeight) && !maxHeightReach)
+				{
+					trace("END OF THE JUMP");
+					velocity.y = 0;
+					maxHeightReach = true;
+				}*/
+
 		}
+
 		if (FlxG.keys.anyJustReleased([Tweaking.moveUp]))
 		{
 			JumpC = 0;
@@ -136,22 +188,22 @@ class Player extends FlxSprite
 			if (jumpTimer.elapsedTime < 0.5)
 			{
 				trace("DOWN");
-				velocity.y += 9.81 * 9.81 * 3;
+				velocity.y = 0; //* 9.81 * 3;
 			}
+
+			maxHeightReach = false;
 		}
-		
+
 		if (velocity.x != 0)
 		{
 			if (moveDown)
 			{
-				
 				animation.play("crouchWalk");
 			}
 			else
 			{
-				animation.play("walk");	
+				animation.play("walk");
 			}
-			
 		}
 		else
 		{
@@ -159,26 +211,131 @@ class Player extends FlxSprite
 			{
 				animation.play("idle");
 			}
-			
 		}
-		
-		
+
+		/* JUMP ANIMATION */
+		//trace("Velocity Y  : " + velocity.y);
 		if (velocity.y < 0)
 		{
+
 			animation.play("jump");
 		}
 		else if (velocity.y > 0)
 		{
+			//trace("HIGH : " + -(this.y - initialHeight));
 			animation.play("fall");
+
 		}
-		
+
 	}
-	
-	
-	
-	
+
 	private function move():Void
 	{
+		/**DEBUG SECTION **\
+		 ******************/
+
+		if (FlxG.keys.anyPressed([FlxKey.SHIFT]))
+		{
+			if (FlxG.keys.anyPressed([FlxKey.J]))
+			{
+				canDoubleJump = true;
+			}
+		}
+	
+	
+	
+	//IS GROUNDED
+	
+		if (isTouching(FlxObject.FLOOR))
+		{
+			jumpC = 0;
+			if (isJumping)
+			{
+				isJumping = false;
+				//animation.play("afterfall");
+			}
+		}
+	
+	
+
 		
+		
+		
+		/**ATTACK**/
+		
+		if (FlxG.keys.anyPressed([FlxKey.RIGHT]))
+		{
+			if (!weaponSprite.visible)
+			{
+				weaponSprite.visible = true;
+			}
+		}
+		if (FlxG.keys.anyJustReleased([FlxKey.RIGHT]))
+		{
+			if (weaponSprite.visible)
+			{
+				weaponSprite.visible = false;
+			}
+		}
+		
+		
+		
+		
+		
+		/**MOVE**/
+		
+		
+		var moveLeft:Bool = FlxG.keys.anyPressed([Tweaking.moveLeft]);
+		var moveRight:Bool = FlxG.keys.anyPressed([Tweaking.moveRight]);
+
+		acceleration.x = 0;
+
+		if (moveLeft)
+		{
+			acceleration.x += -horizontalSpeed ;
+			facing = FlxObject.LEFT;
+		}
+		if (moveRight)
+		{
+			acceleration.x += horizontalSpeed ;
+			facing = FlxObject.RIGHT;
+		}
+
+		if (FlxG.keys.anyJustPressed([Tweaking.moveUp]))
+		{
+			trace(" Debut du Jump");
+			jumpTimer.start(100);
+			initialHeight = this.y;
+			isJumping = true;
+		}
+
+		if (FlxG.keys.anyPressed([Tweaking.moveUp]))
+		{
+			if (isTouching(FlxObject.FLOOR) && jumpC == 0)
+			{
+				velocity.y = -jumpingVelocity;
+				jumpC++;
+				buttonJumpRelease = false;
+			}
+
+			if (canDoubleJump)
+			{
+				if (jumpC == 1 && buttonJumpRelease)
+				{
+					velocity.y = -jumpingVelocity;
+					jumpC++;
+					buttonJumpRelease = false;
+				}
+			}
+		}
+
+		if (FlxG.keys.anyJustReleased([Tweaking.moveUp]))
+		{
+			//JumpC = 0;
+			doubleJump++;
+			maxHeightReach = false;
+			buttonJumpRelease = true;
+		}
+
 	}
 }
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -4,17 +4,28 @@ import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.editors.tiled.TiledMap;
 import flixel.group.FlxGroup;
+import flixel.math.FlxPoint;
 import flixel.text.FlxText;
+import flixel.tile.FlxTile;
+import flixel.tile.FlxTilemap;
+import flixel.util.FlxColor;
+import flixel.util.FlxStringUtil;
+import haxe.Json;
+import sys.io.File;
+
 
 class PlayState extends FlxState
 {
 	public var level:TiledLevel;
+	public var maps:FlxTilemap;
+	
 	
 	public var score:FlxText;
 	public var status:FlxText;
 	public var coins:FlxGroup;
-	public var player:FlxSprite;
+	public var player:Player;
 	public var floor:FlxObject;
 	public var exit:FlxSprite;
 	
@@ -22,87 +33,102 @@ class PlayState extends FlxState
 	
 
 	
+	public var items:FlxSprite;
+	
 	
 	override public function create():Void 
 	{
+		super.create();
+		//player = new Player(100, 50);
 		
-		FlxG.mouse.visible = false;
+		maps = GenerateLevel();
+		
+		/*
+		var playerPos:Array<FlxPoint> = maps.getTileCoords(3, false);
+		player = new Player(playerPos[0].x, playerPos[0].y);*/
+		
+		
+		
+		add(maps);
+		add(player);
+		//add(player);
+		
+		
+		
+		//UTILE POUR LE DEBUG
+		FlxG.mouse.visible = true;
 		
 		bgColor = 0xffaaaaaa;
 		
 		// Load the level's tilemaps
-		coins = new FlxGroup();
-		level = new TiledLevel("assets/tiled/level.tmx", this);
+		//coins = new FlxGroup();
+		//level = new TiledLevel("assets/tiled/level.tmx", this);
 		
 		// Add backgrounds
-		add(level.backgroundLayer);
+		//add(level.backgroundLayer);
 
 		// Draw coins first
-		add(coins);
+		//add(coins);
 		
 		// Add static images
-		add(level.imagesLayer);
+		//add(level.imagesLayer);
 		
 		// Load player objects
-		add(level.objectsLayer);
+		//add(level.objectsLayer);
+		//add(level.objectsLayer.
 		
 		// Add foreground tiles after adding level objects, so these tiles render on top of player
-		add(level.foregroundTiles);
+		//add(level.foregroundTiles);
 		
 		// Create UI
-		score = new FlxText(2, 2, 80);
-		score.scrollFactor.set(0, 0); 
-		score.borderColor = 0xff000000;
-		score.borderStyle = SHADOW;
-		score.text = "SCORE: " + (coins.countDead() * 100);
-		add(score);
+		//score = new FlxText(2, 2, 80);
+		//score.scrollFactor.set(0, 0); 
+		//score.borderColor = 0xff000000;
+		//score.borderStyle = SHADOW;
+		//score.text = "SCORE: " + (coins.countDead() * 100);
+		//add(score);
+		//
+		//status = new FlxText(FlxG.width - 160 - 2, 2, 160);
+		//status.scrollFactor.set(0, 0);
+		//status.borderColor = 0xff000000;
+		//score.borderStyle = SHADOW;
+		//status.alignment = RIGHT;
+		//status.text = youDied ? "Aww, you died!" : "Collect coins.";
+		//add(status);
+		//
 		
-		status = new FlxText(FlxG.width - 160 - 2, 2, 160);
-		status.scrollFactor.set(0, 0);
-		status.borderColor = 0xff000000;
-		score.borderStyle = SHADOW;
-		status.alignment = RIGHT;
-		status.text = youDied ? "Aww, you died!" : "Collect coins.";
-		add(status);
+		
+		/*ESSAI CDB */
+		//var file = File.getContent("assets/data/new.cdb");
+		//var o = Json.parse("pic");
+		//trace(o);
+		
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
 		player.acceleration.x = 0;
 		
-		if (FlxG.keys.anyPressed([LEFT, A]))
-		{
-			player.acceleration.x -= player.maxVelocity.x * 4;
-		}
-		if (FlxG.keys.anyPressed([RIGHT, D]))
-		{
-			player.acceleration.x += player.maxVelocity.x * 4;
-		}
-		//if (FlxG.keys.anyPressed([W, UP, SPACE]) )
-		//{
-			//doubleJump++;
-			//if (player.isTouching(FlxObject.FLOOR) || doubleJump < 2)
-			//{
-				//player.velocity.y = -player.maxVelocity.y / 2;
-				//
-				//
-			//}
-			//
-		//}
+		FlxG.collide(player, maps);
+		FlxG.overlap(items, player, getItem);
+		
+		
 		super.update(elapsed);
 		
-		FlxG.overlap(coins, player, getCoin);
+		
 		
 		// Collide with foreground tile layer
-		level.collideWithLevel(player);
+		//level.collideWithLevel(player);
 		
-		FlxG.overlap(exit, player, win);
 		
-		if (FlxG.overlap(player, floor))
+		
+		//FlxG.overlap(exit, player, win);
+		
+		/*if (FlxG.overlap(player, floor))
 		{
 			youDied = true;
 			FlxG.resetState();
-		}
+		}*/
 	}
 	
 	public function win(Exit:FlxObject, Player:FlxObject):Void
@@ -112,14 +138,62 @@ class PlayState extends FlxState
 		player.kill();
 	}
 	
-	public function getCoin(Coin:FlxObject, Player:FlxObject):Void
+	public function getItem(Item:FlxObject, Player:FlxObject):Void
 	{
-		Coin.kill();
-		score.text = "SCORE: " + (coins.countDead() * 100);
-		if (coins.countLiving() == 0)
+		Item.kill();
+		player.canDoubleJump = true;
+		//score.text = "SCORE: " + (coins.countDead() * 100);
+	/*	if (coins.countLiving() == 0)
 		{
 			status.text = "Find the exit";
 			exit.exists = true;
-		}
+		}*/
 	}
+	
+	public function GenerateLevel():FlxTilemap
+	{
+		var mapTable = [FlxColor.WHITE, FlxColor.BLACK, FlxColor.RED, FlxColor.BLUE];
+		var mps = new FlxTilemap();
+		var atile = new FlxTile(mps, 1, 16, 16, true, FlxObject.ANY);
+	
+
+		var mapCSV = FlxStringUtil.imageToCSV("assets/data/map.png", false,1,mapTable);
+		
+		var mapTilePath:String = "assets/images/tO.png";
+		mps.loadMapFromCSV(mapCSV, mapTilePath, 16, 16);
+		//mps.loadMapFromGraphic("assets/data/map.png", false, 1, [FlxColor.WHITE, FlxColor.BLACK, FlxColor.RED,FlxColor.YELLOW], mapTilePath, 16, 16, OFF, 0, 1, 1);
+		
+		
+		
+		//Chargement de la position de départ du joueur
+		var playerPos:Array<FlxPoint> = mps.getTileCoords(3, false);
+		player = new Player(playerPos[0].x, playerPos[0].y);
+		
+		
+		//Chargement de la position des objets et instanciation des objets
+		var itemPos:Array<FlxPoint> = mps.getTileCoords(2, false);
+		for (i in itemPos)
+		{
+			items = new FlxSprite(itemPos[0].x, itemPos[0].y);
+			items.loadGraphic("assets/images/batery.png");
+			add(items);
+		}
+		
+		
+		//Remove de la case propre au joueur
+		var playerTiles:Array<Int> = mps.getTileInstances(3);
+		var playerTile:Int = playerTiles[0];
+		mps.setTileByIndex(playerTile, -1, true);
+		
+		var itemTiles:Array<Int> = mps.getTileInstances(2);
+		var itemTile:Int = itemTiles[0];
+		mps.setTileByIndex(itemTile, -1, true);
+		
+		
+		
+		
+		return mps;
+	}
+	
+	
 }
